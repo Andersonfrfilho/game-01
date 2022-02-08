b8a2fd8ef4d3993e70bb5670511cdcc13ab2a6f1
Anderson <andersonfrfilho@gmail.com>
2022-02-08 16:12:18 -0300

    feat: adjust itens

--- src/com/miyazaki/entities/Entity.java
+++ src/com/miyazaki/entities/Entity.java
@@ -1,46 +1,62 @@
 package com.miyazaki.entities;
 
+import com.miyazaki.main.Game;
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-	protected double x;
-	protected double y;
-	protected int width;
-	protected int height;
-	private BufferedImage sprite;
-	public Entity(int x,int y, int width,int height,BufferedImage sprite) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.sprite = sprite;
-	}
-	public void setX(double newX){
-		this.x = newX;
-	}
-	public void setY(double newY){
-		this.y = newY;
-	}
-	public int getX() {
-		return (int)this.x;
-	}
-	public int getY() {
-		return (int)this.y;
-	}
-	public int getWidth() {
-		return this.width;
-	}
-	public int getHeight() {
-		return this.height;
-	}
-	public void render(Graphics g) {
-		g.drawImage(sprite, this.getX(), this.getY(), null);
-	}
-	public void tick() {
-	
-	}
-	public void render() {
-		
-	}
+    public static BufferedImage LIFEPACK_EN = Game.spritesheet.getSprite(9 * 12, 0, 16, 16);
+    public static BufferedImage WEAPON_EN = Game.spritesheet.getSprite(10 * 12, 0, 16, 16);
+    public static BufferedImage BULLET_EN = Game.spritesheet.getSprite(9 * 12, 16, 16, 16);
+    public static BufferedImage ENEMY_EN = Game.spritesheet.getSprite(10 * 12, 16, 16, 16);
+    protected double x;
+    protected double y;
+    protected int width;
+    protected int height;
+    private BufferedImage sprite;
+
+    public Entity(int x, int y, int width, int height, BufferedImage sprite) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.sprite = sprite;
+    }
+
+    public void setX(double newX) {
+        this.x = newX;
+    }
+
+    public void setY(double newY) {
+        this.y = newY;
+    }
+
+    public int getX() {
+        return (int) this.x;
+    }
+
+    public int getY() {
+        return (int) this.y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void render(Graphics g) {
+        g.drawImage(sprite, this.getX(), this.getY(), null);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
 }
