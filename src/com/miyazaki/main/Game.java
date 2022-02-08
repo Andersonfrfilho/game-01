907b8959a0c6d4ea5d26a5011040922322dede97
Anderson <andersonfrfilho@gmail.com>
2022-02-08 01:36:47 -0300

    :construct:: add tilesheet map

--- src/com/miyazaki/main/Game.java
+++ src/com/miyazaki/main/Game.java
@@ -16,150 +16,159 @@ import javax.swing.JFrame;
 import com.miyazaki.entities.Entity;
 import com.miyazaki.entities.Player;
 import com.miyazaki.graficos.Spritesheet;
+import com.miyazaki.world.World;
 
-public class Game extends Canvas implements Runnable,KeyListener{
-	
-	private static final long serialVersionUID = 1L;
-	public static JFrame frame;
-	private final int WIDTH = 240;
-	private final int HEIGHT = 160;
-	private final int SCALE = 3;
-	private boolean isRunning = true;
-	private Thread thread;
-	
+public class Game extends Canvas implements Runnable, KeyListener {
 
-	private BufferedImage image;
-	
-	public List<Entity> entities;
-	public static Spritesheet spritesheet;
-	private Player player;
-	
-	public Game() {
-		addKeyListener(this);
-		this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
-		initFrame();
-		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
-		entities = new ArrayList<Entity>();
-		spritesheet = new Spritesheet("/spritesheet.png");
-		player = new Player(32,0,16,16,spritesheet.getSprite(32,0,16,16));
-		entities.add(player);
-	}
-	
-	public void initFrame() {
-		frame = new JFrame("Game #1");
-		frame.add(this);
-		frame.setResizable(false);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-	}
-	
-	public synchronized void start() {
-		thread = new Thread(this);
-		isRunning = true;
-		thread.start();
-	}
-	
-	public synchronized void stop() {
-		isRunning = false;
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String args[]) {
-		Game game = new Game();
-		game.start();
-	}
-	
-	public void tick() {
-		for(int i=0; i< entities.size();i++) {
-			Entity e = entities.get(i);
-			e.tick();
-		}
-	}
-	public void render() {
-		BufferStrategy bs = this.getBufferStrategy();
-		if(bs == null) {
-			this.createBufferStrategy(3);
-			return;
-		}
-		Graphics g = image.getGraphics();
-		g.setColor(new Color(0,255,0));
-		g.fillRect(0, 0, WIDTH, HEIGHT);
-		//renderiza��o do jogo
-		//Graphics2D g2 = (Graphics2D) g;
-		for(int i=0; i< entities.size();i++) {
-			Entity e = entities.get(i);
-			e.render(g);
-		}
-		//final
-		g.dispose();
-		g = bs.getDrawGraphics();
-		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
-		bs.show();
-	}
-	public void run() {
-		long lastTime = System.nanoTime();
-		final double AMOUNT_OF_TICKS = 60.0;
-		final double NS = 1000000000 / AMOUNT_OF_TICKS;
-		double delta = 0;
-		int frames = 0;
-		double timer = System.currentTimeMillis();
-		while(isRunning) {
-			long now = System.nanoTime();
-			delta += (now-lastTime) / NS;
-			lastTime = now;
-			if(delta >= 1) {
-				tick();
-				render();
-				frames++;
-				delta--;
-			}
-			
-			if(System.currentTimeMillis() - timer >= 1000) {
-				System.out.println("FPS: "+frames);
-				frames = 0;
-				timer+=1000;
-			}
-		}
-		stop();
-	}
+    private static final long serialVersionUID = 1L;
+    public static JFrame frame;
+    private final int WIDTH = 240;
+    private final int HEIGHT = 160;
+    private final int SCALE = 3;
+    private boolean isRunning = true;
+    private Thread thread;
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    private BufferedImage image;
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT||e.getKeyCode()==KeyEvent.VK_D) {
-			player.right = true;
-		}else if(e.getKeyCode()==KeyEvent.VK_LEFT||e.getKeyCode()==KeyEvent.VK_A) {
-			player.left = true;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_UP||e.getKeyCode()==KeyEvent.VK_W) {
-			player.up = true;
-		}else if(e.getKeyCode()==KeyEvent.VK_DOWN||e.getKeyCode()==KeyEvent.VK_S) {
-			player.down = true;
-		}
-	}
+    public List<Entity> entities;
+    public static Spritesheet spritesheet;
+    private Player player;
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_RIGHT||e.getKeyCode()==KeyEvent.VK_D) {
-			player.right = false;
-		}else if(e.getKeyCode()==KeyEvent.VK_LEFT||e.getKeyCode()==KeyEvent.VK_A) {
-			player.left = false;
-		}
-		if(e.getKeyCode()==KeyEvent.VK_UP||e.getKeyCode()==KeyEvent.VK_W) {
-			player.up = false;
-		}else if(e.getKeyCode()==KeyEvent.VK_DOWN||e.getKeyCode()==KeyEvent.VK_S) {
-			player.down = false;
-		}
-	}
+    public static World world;
+
+    public Game() {
+        addKeyListener(this);
+        this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        initFrame();
+        //Inicializando objetos
+
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        entities = new ArrayList<Entity>();
+        spritesheet = new Spritesheet("/spritesheet.png");
+        world = new World("/map.png");
+        player = new Player(32, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16));
+        entities.add(player);
+
+    }
+
+    public void initFrame() {
+        frame = new JFrame("Game #1");
+        frame.add(this);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        Game game = new Game();
+        game.start();
+    }
+
+    public void tick() {
+        for (int i = 0; i < entities.size(); i++) {
+            Entity e = entities.get(i);
+            e.tick();
+        }
+    }
+
+    public void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+        Graphics g = image.getGraphics();
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        //renderiza��o do jogo
+        //Graphics2D g2 = (Graphics2D) g;
+        world.render(g);
+        for (int i = 0; i < entities.size(); i++) {
+            Entity e = entities.get(i);
+            e.render(g);
+        }
+        //final
+        g.dispose();
+        g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+        bs.show();
+    }
+
+    public void run() {
+        long lastTime = System.nanoTime();
+        final double AMOUNT_OF_TICKS = 60.0;
+        final double NS = 1000000000 / AMOUNT_OF_TICKS;
+        double delta = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
+        while (isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / NS;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if (System.currentTimeMillis() - timer >= 1000) {
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer += 1000;
+            }
+        }
+        stop();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            player.right = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            player.left = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+            player.up = true;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            player.down = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            player.right = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            player.left = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+            player.up = false;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            player.down = false;
+        }
+    }
 }
---
+++ src/com/miyazaki/world/Tile.java
@@ -0,0 +1,23 @@
+package com.miyazaki.world;
+
+import com.miyazaki.main.Game;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    public static BufferedImage TILE_FLOOR = Game.spritesheet.getSprite(0,0,16,16);
+    public static BufferedImage TILE_WALL = Game.spritesheet.getSprite(16,0,16,16); //static faz inicializar apenas uma vez
+
+    private BufferedImage sprite;
+    private int x,y;
+
+    public Tile(int x,int y, BufferedImage sprite){
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+    }
+    public void render(Graphics g){
+        g.drawImage(sprite,x,y,null);
+    }
+}
